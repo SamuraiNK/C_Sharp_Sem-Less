017393311fbc3ebd11fddcc30062537152d76383
Samurai <samurai230613@gmail.com>
2023-07-13 14:20:24 +0300

    Finish commit

--- sem_2/Program.cs
+++ sem_2/Program.cs
@@ -1,8 +1,92 @@
-﻿int t = 5;
-int num = 3;
+﻿// int t = 5;
+// int num = 3;
 
 // Канкатинация
-Console.WriteLine("Число " +t+ " больше, чем " +num );
+// Console.WriteLine("Число " +t+ " больше, чем " +num );
 
 // Интерполяция 
-Console.WriteLine($"Число {t} больше, чем {num}");
+//Console.WriteLine($"Число {t} больше, чем {num}");
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int num= new Random().Next(10,100);
+// Console.WriteLine(num);
+
+// int num1= num / 10;
+// int num2= num % 10;
+
+// if(num1>num2)
+// {
+// Console.WriteLine(num1);
+// }
+// else if(num1==num2)
+// {
+// Console.WriteLine(num1);
+// }
+// else
+// {
+// Console.WriteLine(num2);
+// }
+
+// int number = new Random().Next(100, 1000); // 10 -99
+// Console.WriteLine(number);
+
+// int number1 = number / 100;
+// int number2 = number % 10;
+// Console.WriteLine($"{number1}{number2}");
+
+
+
+
+// Console.WriteLine ("Введите первое число ");
+// int number_1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine ("Введите второе число ");
+// int number_2 = Convert.ToInt32(Console.ReadLine());
+
+// if (number_1 % number_2 == 0)
+// {
+//     Console.WriteLine ("Кратны ");
+
+// }
+// else 
+// {
+//     Console.WriteLine ($"Не кратно, остаток {number_1 % number_2} ");
+// }
+
+
+
+// && - Выполняется оба условия
+// || - Выполнятеся любое из условий 
+
+
+// Console.WriteLine("Введите число ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// // if((number %7 == 0)||(number % 23 == 0 ))
+// // {
+// //     Console.WriteLine("Да");
+
+// else
+// // {
+// //     Console.WriteLine("Нет");
+// // }
+
+
+
+// Console.WriteLine ("Введите первое число ");
+// int number_1 = Convert.ToInt32(Console.ReadLine ());
+
+// Console.WriteLine ("Введите второче число ");
+// int number_2 = Convert.ToInt32(Console.ReadLine ());
+
+// if ((number_1 == number_2 * number_2) || (number_2 == number_1 * number_1 ))
+// {
+//     Console.WriteLine ("Да");
+// }
+// else
+// {
+//    Console.WriteLine ("Неn");
+// }
+
